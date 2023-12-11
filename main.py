1780e8c459cef98a50c7f2d42b2ce866cac81aa7
lyc2001 <liniris2001@gmail.com>
2023-12-11 23:24:20 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+from flask_ngrok import run_with_ngrok
+from flask import Flask, render_template, request
+
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+import torch
+import base64
+from io import BytesIO
+
+
+
+model = "lemonpuddi/llama-2-7b-miniguanaco"
+tokenizer = AutoTokenizer.from_pretrained(model)
+
+falcon_pipeline = transformers.pipeline("text-generation",
+                                        model=model,
+                                        tokenizer=tokenizer,
+                                        torch_dtype=torch.bfloat16,
+                                        trust_remote_code=True,
+                                        device_map=0
+                                        )
+
+
+# define completion function
+def get_completion_falcon(input):
+  prompt = f"<s>[INST] {input} [/INST]"
+  #print(prompt)
+  falcon_response = falcon_pipeline(prompt,
+                                    max_length=500,
+                                    do_sample=True,
+                                    top_k=10,
+                                    num_return_sequences=1,
+                                    eos_token_id=tokenizer.eos_token_id,
+                                    )
+
+  return falcon_response
+
+# Start flask app and set to ngrok
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route('/')
+def initial():
+  return render_template('index.html')
+
+@app.route('/submit-prompt', methods=['POST'])
+def generate_text():
+    prompt = request.form['prompt-input']
+    print(f"Generating text for prompt: {prompt}")
+
+    # Call the text generation function
+    response = get_completion_falcon(prompt)
+    generated_text = response[0]['generated_text']
+    print("Text generated!")
+
+    print("Sending text ...")
+    return render_template('index.html', generated_text=generated_text)
+
+
+
+
+if __name__ == '__main__':
+    app.run()
